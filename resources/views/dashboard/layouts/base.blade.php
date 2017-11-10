0fde7b15cacc85f79f847f0ab637b4917c6ae350
Yoga Hanggara <yoga@javan.co.id>
2017-11-10 15:03:10 +0700

    tambah template

--- resources/views/dashboard/layouts/base.blade.php
+++ resources/views/dashboard/layouts/base.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <title>{{ config('app.name') }}</title>
     <!-- Vendor styles -->
     <link rel="stylesheet" href="{{ asset('template/vendors/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css') }}">
     <link rel="stylesheet" href="{{ asset('template/vendors/bower_components/animate.css/animate.min.css') }}">
