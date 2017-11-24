648f2fca5f999e7ab33ba75fed866b13cbe3fe7f
Annisa Ghani <annisa.ghani.permana@gmail.com>
2017-11-24 10:31:05 +0700

    commit issue

---
+++ app/Http/Requests/SpeciesRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SpeciesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'scientific_name' => 'required|string|max:100',
+            'indonesia_name' => 'required|string|max:100',
+            'general_name' => 'required|string|max:100',
+        ];
+    }
+}
