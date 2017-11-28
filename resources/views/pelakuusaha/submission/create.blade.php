a4d0640326eb6d3b17bf2480a4e4d21f6a6afca9
Annisa Ghani <annisa.ghani.permana@gmail.com>
2017-11-28 08:33:10 +0700

    perbaiki alert kuota 0

--- resources/views/pelakuusaha/submission/create.blade.php
+++ resources/views/pelakuusaha/submission/create.blade.php
@@ -14,7 +14,7 @@
                 </div>
                 <div class="card-block">
                     <?php
-                        /*$tahun=date('Y');
+                        $tahun=date('Y');
                         $tambah=mktime(0,0,0,date('m')+6,date('d')+0,date('Y')+0);
                         $cek=date('Y', $tambah);
                         if($cek>$tahun){
@@ -24,7 +24,7 @@
                                     <p>Waktu pengajuan tidak sesuai, silahkan mengajukan permohonan pada awal tahun atau mengajukan permohonan bertingkat.</p>
                                 </div>
                             ';
-                        }*/
+                        }
                     ?>
                     @include('includes.notifications')
 
@@ -35,7 +35,7 @@
 
                         <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-14">
-                                <button type="submit" class="btn btn-primary" <?php //if($cek>$tahun){echo 'disabled title="tidak diizinkan melakukan permohonan langsung"';}?>>Simpan Baru</button>
+                                <button type="submit" class="btn btn-primary" <?php if($cek>$tahun){echo 'disabled title="tidak diizinkan melakukan permohonan langsung"';}?>>Simpan Baru</button>
                                 <a href="{{ route('user.submission.index') }}" class="btn btn-default">Batal</a>
                             </div>
                         </div>
