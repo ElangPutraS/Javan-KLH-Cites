f9ca0a0178923ac08c0ca09e24e074c9bc34c64f
Kahfi Yordan <kyordan@gmail.com>
2017-11-29 12:17:59 +0700

    pengaturan cities seeder

--- database/seeds/CitiesSeeder.php
+++ database/seeds/CitiesSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class CitiesSeeder extends Seeder
 {
@@ -13,22 +12,17 @@ class CitiesSeeder extends Seeder
     public function run()
     {
         //
-    	DB::transaction(function () {
-        	DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-        	DB::statement('TRUNCATE TABLE cities');
+    
 
             $jsonData = json_decode(File::get(database_path('json/cities.json')), JSON_OBJECT_AS_ARRAY);
-        	$faker = Faker::create();
-
         	foreach ($jsonData as $key => $item) {
         		//DB::table('ports')->insert($item);
-        		DB::table('cities')->insert([
-        			'city_code' => $item['city_code'],
-        			'city_name' => $item['city_name'],
+        		Country::create([
+                    'city_code' => $item['city_code'],
+                    'city_name' => $item['city_name'],
                     'city_name_full' => $item['city_name_full'],
-                    'province_id' => $item['province_id'],
-        			'created_at' => $faker->dateTime(),
-        			'updated_at' => date('Y-m-d h:i:s')
+                    'province_id' => $item['province_id']
+                ])
         		]);
         	}
         });
