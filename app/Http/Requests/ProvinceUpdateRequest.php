24dd657af9292037e9b3d7b58b18404e4df1c71b
Ade Ridwan Pahlevi <aderidwanpahlevi@gmail.com>
2017-11-29 12:55:07 +0700

    Kelola Provinsi

---
+++ app/Http/Requests/ProvinceUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProvinceUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'country_id'        => 'required',
+            'province_code'     => 'required|string|max:4',
+            'province_name'     => 'required|string|max:50',
+        ];
+  
+    }
+}
