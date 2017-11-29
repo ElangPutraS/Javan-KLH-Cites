2133a03fb6fb26d4ec9186c99b32a8ebd7ca2ab7
Firma Firmansyah Adi <firmafirmansyah@gmail.com>
2017-11-29 14:09:49 +0700

    perbaiki review

--- app/Http/Controllers/RenewalController.php
+++ app/Http/Controllers/RenewalController.php
@@ -19,91 +19,43 @@ class RenewalController extends Controller
      */
     public function index()
     {
-        $trade_permits=TradePermit::where('trade_permit_status_id','>=',8)->paginate(10);
+        $trade_permits=TradePermit::whereHas('tradeStatus', function ($query) {
+            $query->where('status_code', '>=', '600');
+        })->orderBy('trade_permit_code', 'asc')->paginate(10);
 
         return view('pelakuusaha.renewals.index', compact('trade_permits'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Request $request, $id)
     {
-        $user=User::find($request->user()->id);
-
+        $user=$request->user();
         $trading_types=TradingType::orderBy('trading_type_name', 'asc')->pluck('trading_type_name', 'id');
         $purpose_types=PurposeType::pluck('purpose_type_name', 'id');
         $ports=Ports::orderBy('port_name', 'asc')->pluck('port_name', 'id');
-        $document_types=DocumentType::where('is_permit',1)->orderBy('document_type_name', 'asc')->pluck('document_type_name', 'id');
-        $trade_permit=TradePermit::find($id);
+        $trade_permit=TradePermit::findOrFail($id);
         return view('pelakuusaha.renewals.edit', compact('user', 'trade_permit','trading_types','purpose_types','ports'));
-
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $trade_permit=TradePermit::findOrFail($id);
+        $trade_permit->update([
+            'consignee'=>$request->get('cogsinee'),
+            'appendix_type'=>$request->get('appendix_type'),
+            'period'=>$request->get('period'),
+            'port_exportation'=>$request->get('port_exportation'),
+            'port_destination'=>$request->get('port_destination'),
+            'trading_type_id'=>$request->get('trading_type_id'),
+            'purpose_type_id' =>$request->get('purpose_type_id')
+        ]);
+        return redirect()->route('pelakuusaha.renewals.edit', ['id' => $trade_permit->id])->with('success', 'Data berhasil diubah.');
     }
 
     public function getSubmission(Request $request){
         $trade_permit=TradePermit::where('trade_permit_code',$request->no)->first();
-        if (count($trade_permit)>0){
+        if ($trade_permit){
             return($trade_permit->id);
-        }else{
-            return 0 ;
         }
+            return 0 ;
     }
 }
