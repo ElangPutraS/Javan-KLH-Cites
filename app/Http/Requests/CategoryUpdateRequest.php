6c3f03827f40fe5237498f232676aca6da4ffe26
Annisa Ghani <annisa.ghani.permana@gmail.com>
2017-12-05 09:35:44 +0700

    perbaikan kategori

--- app/Http/Requests/CategoryUpdateRequest.php
+++ app/Http/Requests/CategoryUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CategoryUpdateRequest extends FormRequest
 {
@@ -24,7 +25,10 @@ class CategoryUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'category_code' => 'required|string|max:191',
+            'species_category_code' => [
+                'required', 'string', 'max:191',
+                Rule::unique('categories')->ignore( $this->route('id')),
+            ],
             'category_name' => 'required|string|max:191',
         ];
     }
