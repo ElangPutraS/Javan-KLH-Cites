7685023789860108e0bb07f2ae4d957749ca44ff
Firma Firmansyah Adi <firmafirmansyah@gmail.com>
2017-12-07 14:03:07 +0700

    role super admin

---
+++ resources/views/superadmin/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('dashboard.layouts.base')
+
+@section('content')
+    <section class="content">
+        <div class="content__inner">
+            <header class="content__title">
+                <h1>Pembaharuan SATS-LN</h1>
+            </header>
+
+            <div class="card">
+                <div class="card-block">
+
+                    @include('includes.notifications')
+
+                    <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+
+                        {!! csrf_field() !!}
+
+                        @include('pelakuusaha.renewals._form')
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-14">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                <a href="{{ route('user.renewal.index') }}" class="btn btn-default">Kembali ke Daftar</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+@push('body.script')
+    <script src="{{ asset('template/vendors/bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('template/vendors/bower_components/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('template/vendors/bower_components/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('template/vendors/bower_components/jszip/dist/jszip.min.js') }}"></script>
+    <script src="{{ asset('template/vendors/bower_components/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+@endpush
