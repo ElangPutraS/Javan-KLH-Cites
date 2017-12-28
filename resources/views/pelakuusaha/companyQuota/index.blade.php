3b65dbe10a0108b24e9b5028a14ac6b521c79b8d
Annisa Ghani <annisa.ghani.permana@gmail.com>
2017-12-28 11:22:18 +0700

    ganti nama kuota -> kuota ekspor

--- resources/views/pelakuusaha/companyQuota/index.blade.php
+++ resources/views/pelakuusaha/companyQuota/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <section class="content">
         <header class="content__title">
-            <h1>Kelola Kuota Spesies Perusahaan</h1>
+            <h1>Kelola Kuota Ekspor Perusahaan</h1>
         </header>
 
         <div class="card">
             <div class="card-header">
-                <h2 class="card-title">Daftar Kuota Spesies Perusahaan</h2>
+                <h2 class="card-title">Daftar Kuota Ekspor Perusahaan</h2>
                 <small class="card-subtitle">{{ strtoupper($company->company_name) }}</small>
             </div>
 
