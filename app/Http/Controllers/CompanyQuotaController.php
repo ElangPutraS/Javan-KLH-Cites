83895d23ac03fddc8e99a0cb58f81ca3eb3ba478
Annisa Ghani <annisa.ghani.permana@gmail.com>
2017-12-29 16:47:39 +0700

    filter daftar kuota pemilik usaha

--- app/Http/Controllers/CompanyQuotaController.php
+++ app/Http/Controllers/CompanyQuotaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Company;
+use App\CompanyQuota;
 use Illuminate\Http\Request;
 
 class CompanyQuotaController extends Controller
@@ -10,8 +11,24 @@ class CompanyQuotaController extends Controller
     public function index(Request $request)
     {
         $company = $request->user()->company;
-        $companyQuota = $company->companyQuota()->orderBy('year','desc')->paginate(10);
+        if($request->input('year') == '' && $request->input('scientific_name') == '' && $request->input('indonesia_name') == '' && $request->input('general_name') == '' || $request->input('year') == null && $request->input('scientific_name') == null && $request->input('indonesia_name') == null && $request->input('general_name') == null){
+            $companyQuota = $company->companyQuota()->orderBy('year','desc')->paginate(10);
+        }else{
+            $year='%'.$request->input('year').'%';
+            $scientific_name='%'.$request->input('scientific_name').'%';
+            $indonesia_name='%'.$request->input('indonesia_name').'%';
+            $general_name='%'.$request->input('general_name').'%';
 
-        return view( 'pelakuusaha.companyQuota.index', compact('company', 'companyQuota'));
+            $companyQuota = $company->companyQuota()
+                ->where('year', 'like', $year)
+                ->where('species_scientific_name', 'like', $scientific_name)
+                ->where('species_indonesia_name', 'like', $indonesia_name)
+                ->where('species_general_name', 'like', $general_name)
+                ->orderBy('year','desc')->paginate(10);
+        }
+
+        $years = CompanyQuota::select('year')->distinct()->get();
+
+        return view( 'pelakuusaha.companyQuota.index', compact('company', 'companyQuota', 'years'));
     }
 }
