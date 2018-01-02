0364507ced17b9d58ae387c9082e7297ef0a567b
Annisa Ghani <annisa.ghani.permana@gmail.com>
2018-01-02 09:35:56 +0700

    filter kuota perusahaan admin

--- resources/views/admin/companyQuota/detail.blade.php
+++ resources/views/admin/companyQuota/detail.blade.php
@@ -13,14 +13,47 @@
             </div>
 
             <div class="card-block">
+                <form method="post" enctype="multipart/form-data" class="form-inline" id="form-search">
+                    <div class="input-group col-sm-3">
+                        <span class="input-group-addon" id="basic-month">Nama Ilmiah</span>
+                        <input class="form-control" type="text" placeholder="Cari nama ilmiah.." name="scientific_name" id="scientific_name" value="@if(Request::input('scientific_name')){{Request::input('scientific_name')}} @endif">
+                    </div>
+
+                    <div class="input-group col-sm-3">
+                        <span class="input-group-addon" id="basic-year">Nama Indonesia</span>
+                        <input class="form-control" placeholder="Cari nama indonesia.." type="text" name="indonesia_name" id="indonesia_name" value="@if(Request::input('indonesia_name')){{Request::input('indonesia_name')}} @endif">
+                    </div>
+
+                    <div class="input-group col-sm-3">
+                        <span class="input-group-addon" id="basic-month">Nama Umum</span>
+                        <input class="form-control" type="text" placeholder="Cari nama umum.." name="general_name" id="general_name" value="@if(Request::input('general_name')){{Request::input('general_name')}} @endif">
+                    </div>
+
+                    <div class="input-group col-sm-2">
+                        <span class="input-group-addon" id="basic-year">Tahun</span>
+                        <select name="year" id="year" class="form-control select2" aria-describedby="basic-year">
+                            <option value="">--Pilih--</option>
+                            @foreach($years as $year)
+                                <option value="{{ $year->year }}" {{ Request::input('year') == $year->year ? 'selected' : '' }} > {{ $year->year }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="btn-group col-sm-1" role="group" aria-label="...">
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Cari
+                        </button>
+                    </div>
+                </form><br>
                 @include('includes.notifications')
-                <a href="{{ route('admin.companyQuota.create', ['id' => $company->id]) }}" class="btn btn-primary">Tambah Baru</a>
+                <a href="{{ route('admin.companyQuota.create', ['id' => $company->id]) }}" class="btn btn-primary">Tambah Baru</a><hr>
                 <div class="table-responsive">
                     <table class="table table-bordered table-sm">
                         <thead class="thead-default">
                         <tr>
                             <th>No</th>
-                            <th>Nama Spesies</th>
+                            <th>Nama Ilmiah Spesies</th>
+                            <th>Nama Indonesia Spesies</th>
+                            <th>Nama Umum Spesies</th>
                             <th>Tahun</th>
                             <th>Jumlah Kuota</th>
                             <th>Jumlah yang telah terealisasi</th>
@@ -31,7 +64,9 @@
                         @forelse($companyQuota as $quota)
                             <tr>
                                 <td>{{ (( $companyQuota->currentPage() - 1 ) * $companyQuota->perPage() ) + $loop->iteration }}</td>
-                                <td><i>{{ $quota->species_scientific_name}}</i> ( {{$quota->species_indonesia_name}} )</td>
+                                <td><i>{{ $quota->species_scientific_name}}</i> </td>
+                                <td>{{ $quota->species_indonesia_name }} </td>
+                                <td>{{ $quota->species_general_name }} </td>
                                 <td>{{  $quota->pivot->year }}</td>
                                 <td>{{ $quota->pivot->quota_amount.' '.$quota->unit->unit_description }}</td>
                                 <td>{{ $quota->pivot->realization.' '.$quota->unit->unit_description }}</td>
@@ -60,6 +95,19 @@
 @push('body.script')
     <script src="{{asset('template/vendors/bower_components/sweetalert2/dist/sweetalert2.min.js')}}"></script>
     <script>
+        $(document).ready(function () {
+            $('#form-search').submit(function (ev) {
+                ev.preventDefault();
+
+                var year = $('#year').val();
+                var scientific_name = $('#scientific_name').val();
+                var indonesia_name = $('#indonesia_name').val();
+                var general_name = $('#general_name').val();
+
+                location.href = '?scientific_name=' + scientific_name + '&indonesia_name=' + indonesia_name + '&general_name=' + general_name + '&year=' + year ;
+            });
+        });
+
         function deleteQuota(a) {
             var company=a.getAttribute('data-company');
             var pivot=a.getAttribute('data-pivot');
