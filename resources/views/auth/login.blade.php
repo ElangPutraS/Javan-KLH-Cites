1594b202ebc049a06ff16eb31acad753e69378a8
Firma Firmansyah Adi <firmafirmansyah@gmail.com>
2018-01-31 11:05:40 +0700

    message forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,16 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
+
                     <div class="panel-heading">Login</div>
+
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
                             @include('includes.notifications')
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                 <label for="email" class="col-md-4 control-label">E-Mail</label>
+
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control" name="email"
                                            value="{{ old('email') }}" required autofocus>
