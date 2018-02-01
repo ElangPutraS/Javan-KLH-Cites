a60bca66985c1f26f814c03f1dfac8d22f86af80
Firma Firmansyah Adi <firmafirmansyah@gmail.com>
2018-02-01 14:50:51 +0700

    upload data master

--- app/Http/Controllers/Admin/UploadMasterDataController.php
+++ app/Http/Controllers/Admin/UploadMasterDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
 use App\Species;
 use Excel;
 use App\SpeciesQuota;
@@ -18,7 +19,7 @@ class UploadMasterDataController extends Controller
     public function quotaExcel($type)
     {
         $quota = SpeciesQuota::get()->toArray();
-        return Excel::create('Data Quota', function($excel) use ($quota) {
+        Excel::create('Data Quota', function($excel) use ($quota) {
             $excel->sheet('sheet name', function($sheet) use ($quota)
             {
                 $sheet->fromArray($quota);
@@ -28,19 +29,48 @@ class UploadMasterDataController extends Controller
 
     public function speciesExcel()
     {
-        Excel::create('Form Species', function($excel) {
-            $excel->sheet('Sheet Name', function($sheet) {
-
-                $head = array(
-                    'Title 1',
-                    'Title 2',
-                    'Title 3',
-                    'Title 4'
+        $categories = Category::get()->toArray();
+        Excel::create('Form Upload Spesies', function($excel) use ($categories) {
+            $excel->sheet('Data Kategori Spesies', function($sheet) use ($categories)
+            {
+                $sheet->fromArray($categories);
+                $sheet->setFreeze('A1');
+                $sheet->_parent->addNamedRange(
+                    new \PHPExcel_NamedRange(
+                        'categories', $sheet, 'C:C'
+                    )
                 );
-
-                $data = array($head);
-                $sheet->fromArray($data, null, 'A4', false, false);
             });
+
+            $excel->sheet('Input Data', function ($sheet){
+                $head = array(
+                    'No',
+                    'Scientific Name',
+                    'General Name',
+                    'Indonesia Name',
+                    'Nominal',
+                    'Appendix',
+                    'Source',
+                    'Category'
+                );
+                $objValidation = $sheet->getCell('H2')->getDataValidation();
+                $objValidation->setType(\PHPExcel_Cell_DataValidation::TYPE_LIST);
+                $objValidation->setErrorStyle(\PHPExcel_Cell_DataValidation::STYLE_STOP);
+                $objValidation->setAllowBlank(false);
+                $objValidation->setShowInputMessage(true);
+                $objValidation->setShowErrorMessage(true);
+                $objValidation->setShowDropDown(true);
+                $objValidation->setErrorTitle('Input error');
+                $objValidation->setError('Value is not in list.');
+                $objValidation->setPromptTitle('Pick from list');
+                $objValidation->setPrompt('Please pick a value from the drop-down list.');
+                $objValidation->setFormula1('categories');
+                //note this!
+                $data = array($head);
+                $sheet->fromArray($data, null, 'A1', false, false);
+            });
+            $excel->setActiveSheetIndex(1);
+
         })->download('xlsx');
     }
 }
